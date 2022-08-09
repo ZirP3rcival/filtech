c254b4de3659070c3ced90863fcfcca972a6f9a5
percival <percival.fernandez.22@gmail.com>
2022-08-09 17:01:38 +0800

    chat update

--- pub/chatbox.php
+++ pub/chatbox.php
@@ -14,26 +14,26 @@
                         <h2>FILTECH Chatbox v1</h2>
                     </div>
                     <div class="chat-content chat-scrollbar">
+<?php
+$dsql = mysqli_query($con,"SELECT * from tblsinfo_data where typ='FACULTY' ORDER BY alyas ASC LIMIT $offsetRV, $rowsPerPageRV"); 
+
+				  while($r = mysqli_fetch_assoc($dsql))
+				   {  
+?>                      
                         <div class="author-chat">
                             <h3>Monica <span class="chat-date">10:15 am</span></h3>
                             <p>Hi, what you are doing and where are you gay?</p>
                         </div>
+                        
                         <div class="client-chat">
                             <h3>Mamun <span class="chat-date">10:10 am</span></h3>
                             <p>Now working in graphic design with coding and you?</p>
                         </div>
-                        <div class="author-chat">
-                            <h3>Monica <span class="chat-date">10:05 am</span></h3>
-                            <p>Practice in programming</p>
-                        </div>
-                        <div class="client-chat">
-                            <h3>Mamun <span class="chat-date">10:02 am</span></h3>
-                            <p>That's good man! carry on...</p>
-                        </div>
+<?php } ?>                        
                     </div>
                     <div class="chat-send">
                         <input type="text" placeholder="Type..." />
-                        <span><button type="submit">Send</button></span>
+                        <span><button id="csend" name="csend" type="submit">Send</button></span>
                     </div>
                 </div>
             </div>
